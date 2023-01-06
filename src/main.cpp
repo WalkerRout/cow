fceca59b77d0252d5fb0369758837e9dc3e173cc
walkerrout04@gmail.com <walkerrout04@gmail.com>
2023-01-05 17:31:39 -0800

    Updated cow

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,88 @@
-
+#include <memory>
+#include <vector>
 #include <iostream>
-#include <algorithm>
-#include <ranges>
+#include <functional>
 
-#include "app.h"
+template <typename T>
+struct Cow {
+  template<typename... Args>
+  Cow(Args... args) noexcept: ptr(std::make_shared<T>(args...)) {}
+  Cow(std::shared_ptr<T> p) noexcept: ptr(p) {}
+  Cow(const Cow &c) noexcept: ptr(c.ptr) {}
 
-auto main(void) -> int {
+  auto operator=(const Cow &rhs) const noexcept {
+    ptr = rhs.ptr;
+  }
 
-  std::string s1{"etalpmet ppc"};
-  std::string s2;
+  auto operator=(std::shared_ptr<T> p) const noexcept {
+    ptr = p;
+  }
+
+  auto operator==(const Cow &rhs) const noexcept {
+    return ptr == rhs.ptr;
+  }
+
+  auto operator*() const noexcept {
+    return *ptr;
+  }
+
+  auto pointer() const {
+    auto fmt = "%p";
+    auto data = ptr.get();
+    auto size = std::snprintf(nullptr, 0, fmt, data);
+    std::vector<char> buf(size+1);
+    
+    if (std::snprintf(&buf[0], buf.size(), "%p", ptr.get()) < 0)
+      exit(EXIT_FAILURE);
+    
+    return std::string{buf.begin(), buf.end()};
+  }
+
+  auto compare(const Cow &rhs) const noexcept {
+    return *ptr == *rhs.ptr;
+  }
+
+  auto& write() noexcept {
+    ptr = clone();
+    return ptr;
+  }
+
+  auto clone() noexcept {
+    if (ptr.use_count() == 1)
+      return ptr;
+
+    return std::make_shared<T>(*ptr); // make clone by dereferencing
+  }
+
+  friend auto& operator<<(std::ostream &os, const Cow &c) noexcept {
+    os << *c.ptr;
+    return os;
+  }
+
+private:
+  std::shared_ptr<T> ptr;
+};
+
+auto main() -> int {
+  auto a = Cow<int>(22);
+  auto b = a;
+
+  std::cout << "B is " << b << "\n";
+  std::cout << "A addr " << a.pointer() << "\n";
+  std::cout << "B addr " << b.pointer() << "\n";
+
+  *b.write() = 3;
   
-  std::transform(s1.rbegin(), s1.rend(), std::back_inserter(s2), [](auto c) { 
-    return std::toupper(c); 
-  });
+  std::cout << "B now " << b << "\n";
+  std::cout << "A addr " << a.pointer() << "\n";
+  std::cout << "B addr " << b.pointer() << "\n";
 
-  auto all_letters = std::ranges::all_of(s2.begin(), s2.end(), [](auto c) { 
-    return 'A' <= c && c <= 'Z' || c == ' '; 
-  });
-
-  std::cout << all_letters << "\n";
-  std::cout << s2 << "\n";
+  auto c = b;
+  *b.write() = 4;
+  
+  std::cout << "B now " << b << "\n";
+  std::cout << "B data " << b << ", address " << b.pointer() << "\n";
+  std::cout << "C data " << c << ", address " << c.pointer() << "\n";
 
   return 0;
 }
